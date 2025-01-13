14e5398dbbbe285e8cbaef0a11a55a6812e5705b
Shabareesha <arjunspy491@gmail.com>
2025-01-13 23:25:53 +0530

    updated treeSearch

--- treeSearch.c
+++ treeSearch.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 
 int main(){
-    int arr[10] = {10,20,30,40,50,60,70,80,90,100};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int mid = n/2;
-    int key = 60;
-    if(key == arr[mid]) printf("%d \n", mid);
-    if(key<arr[mid]){
-        for (int i = mid-1; i>=0; i--)
+    int arr[9] = {1,2,3,4,5,6,7,8,9};
+    int id = 5;
+    int left = 0, right = 9-1;
+    while (left <= right)   
+    {
+        int mid = left+(right-left)/2;
+        if(arr[mid] == id) {
+            printf("%d",mid);
+            return 0;
+        }
+        if (id > arr[mid])
         {
-            if(key == arr[i]) printf("%d \n", i);
-        } 
-    }
-    if (key > arr[mid]){
-        for (int i = mid+1; i < n; i++){
-            if (key == arr[i]){
-                printf("%d \n", i);
-            }
+            left = mid+1;
+        }else{
+            right = mid-1;
         }
     }
-    return 0;
+    
 }
